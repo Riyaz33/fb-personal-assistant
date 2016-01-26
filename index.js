9a9f57f776a0321ae7bbc2bf8f4102168b5be5f8
Kashav Madan <kshvmdn@gmail.com>
2016-01-26 10:52:07 -0500

    Add parse for task add

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ login({email: config.fb.e, password: config.fb.p}, function(err, api) {
   if (err) return console.error(err);
   api.listen(function callback(err, event) {
     //  console.log(event);
-    api.sendMessage('Welcome', event.threadID);
+    // api.sendMessage('Welcome', event.threadID);
+    var eventBody = event.body.toLowerCase();
+    if (eventBody.includes('@bot ')) {
+      // add is of form '@bot add task "subject" "task" "date"'
+      var msg = eventBody.slice('@bot '.length);
+      if (msg.includes('add task ')) {
+        msg = msg.slice('add task '.length).split('\"');
+        var subj = msg[1],
+            task = msg[3],
+            date = msg[5];
+      }
+    }
   })
 });
