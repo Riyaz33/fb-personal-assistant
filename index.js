f621f7dc555934c32b5fa8d2bfa5f3a20ab98617
Kashav Madan <kshvmdn@gmail.com>
2016-01-26 11:16:30 -0500

    Convert date to unix seconds

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Tasks = require('./models/task');
 login({email: config.fb.e, password: config.fb.p}, function(err, api) {
   if (err) return console.error(err);
   api.listen(function callback(err, event) {
-    //  console.log(event);
+    console.log(event);
     // api.sendMessage('Welcome', event.threadID);
     var eventBody = event.body.toLowerCase();
     if (eventBody.includes('@bot ')) {
@@ -17,16 +17,12 @@ login({email: config.fb.e, password: config.fb.p}, function(err, api) {
       var msg = eventBody.slice('@bot '.length);
       if (msg.includes('add task ')) {
         msg = msg.slice('add task '.length).split('\"');
-        var subj = msg[1],
-            task = msg[3],
-            date = msg[5]; // YYYY-MM-DD
         Tasks.create({
-            threadID: event.threadID, 
-            senderID: event.senderID, 
-            taskName: task, 
-            taskSubject: subj, 
-            addDate: moment(), 
-            dueDate: moment(date),
+            threadID: event.threadID,
+            taskName: msg[3],
+            taskSubject: msg[1],
+            addDate: moment().unix(), 
+            dueDate: moment(msg[5]).unix(),
           }, function(err, message) {
             if (err) return console.error(err);
             console.log('Task added')
--- models/task.js
+++ models/task.js
@@ -4,11 +4,10 @@ const db = mongoose.createConnection('mongodb://localhost/fb-assistant');
 
 const TaskSchema = new mongoose.Schema({
   threadID: String,
-  senderID: String,
   taskName: String,
   taskSubject: String,
-  addDate: String,
-  dueDate: String,
+  addDate: Number,
+  dueDate: Number,
 }, {strict: false});
 
 module.exports = db.model('tasks', TaskSchema);
