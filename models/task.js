e9b1820e54385a67f47b548b3228fe4111ca421b
Kashav Madan <kshvmdn@gmail.com>
2016-01-26 10:51:49 -0500

    Update Schema

--- models/task.js
+++ models/task.js
@@ -1,9 +1,14 @@
 'use strict';
 const mongoose = require('mongoose');
-const db = mongoose.createConnect('mongodb://localhost/fb-assistant');
+const db = mongoose.createConnection('mongodb://localhost/fb-assistant');
 
 const TaskSchema = new mongoose.Schema({
-  
+  threadID: String,
+  senderID: String,
+  taskName: String,
+  taskSubject: String,
+  addDate: Number,
+  dueDate: Number,
 }, {strict: false});
 
 module.exports = db.model('tasks', TaskSchema);
