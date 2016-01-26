205596c39a03e092e68bc312e74a6e7834eafc25
Kashav Madan <kshvmdn@gmail.com>
2016-01-26 11:46:50 -0500

    Add method to show tasks for {subj}

--- index.js
+++ index.js
@@ -2,32 +2,50 @@
 
 const login = require('facebook-chat-api');
 const moment = require('moment');
-const config = require('./config');
 const Tasks = require('./models/task');
 
+const config = require('./config');
+const utils = require('./utils');
 
 login({email: config.fb.e, password: config.fb.p}, function(err, api) {
   if (err) return console.error(err);
   api.listen(function callback(err, event) {
-    console.log(event);
-    // api.sendMessage('Welcome', event.threadID);
+    // console.log(event);
     var eventBody = event.body.toLowerCase();
     if (eventBody.includes('@bot ')) {
       // add is of form '@bot add task "subject" "task" "date"'
       var msg = eventBody.slice('@bot '.length);
       if (msg.includes('add task ')) {
         msg = msg.slice('add task '.length).split('\"');
+        // date must be of form YYYY-MM-DD (TODO natural-lang date processing)
         Tasks.create({
             threadID: event.threadID,
             taskName: msg[3],
             taskSubject: msg[1],
             addDate: moment().unix(), 
-            dueDate: moment(msg[5]).unix(),
+            dueDate: moment(utils.parseDate(msg[5])).unix(),
           }, function(err, message) {
             if (err) return console.error(err);
-            console.log('Task added')
+            // console.log('Task added')
+            api.sendMessage('Task added', event.threadID);
           }
         );
+      } else if (msg.includes('show tasks')) {
+        msg = msg.slice('show tasks'.length);
+        if (!utils.isEmpty(msg)) {
+          var subj = msg.slice(' for '.length).split('\"')[1];
+          Tasks.find({ taskSubject: subj }, function(err, tasks) {
+            if (err) return console.error(err);
+            var out;
+            if (utils.isEmpty(tasks)) 
+              return api.sendMessage('No tasks for ' + subj, event.threadID);
+            tasks.forEach(function(task) {
+              api.sendMessage(task.taskName + " | due " + moment(task.dueDate, 'x'), event.threadID);
+            })
+          });
+        } else {
+          console.log('Show all tasks');
+        }
       }
     }
   })
