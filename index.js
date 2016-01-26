1b4f7c22d968840cf26305b28a8e738cacda5e2c
Kashav Madan <kshvmdn@gmail.com>
2016-01-26 13:27:50 -0500

    Add bot help, change name to TaskBot

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ login({email: config.fb.e, password: config.fb.p}, function(err, api) {
   api.listen(function callback(err, event) {
     // console.log(event);
     var eventBody = event.body.toLowerCase();
-    if (eventBody.includes('@bot ')) {
-      // add is of form '@bot add task "subject" "task" "date"'
-      var msg = eventBody.slice('@bot '.length);
-      if (msg.includes('add task ')) {
+    if (eventBody === '@taskbot') {
+      api.sendMessage('Welcome to TaskBot.\nType "@taskbot help" for a list of commands.', event.threadID);
+    }
+    if (eventBody.includes('@taskbot ')) {
+      var msg = eventBody.slice('@taskbot '.length);
+      if (msg.includes('help')) {
+        var out = '';
+        out += 'TaskBot Commands\n\n';
+        out += '@taskbot add task "subject" "task" "due date" - add new task\n';
+        out += '@taskbot show tasks - view pending tasks\n';
+        out += '@taskbot show tasks for "subject" - view pending tasks for subject\n';
+        out += '@taskbot remove "subject" "task name" - remove task';
+        api.sendMessage(out, event.threadID);
+      } else if (msg.includes('add task ')) {
+        // create tasks w/ '@taskbot add task "subject" "task" "date"'
         msg = msg.slice('add task '.length).split('\"');
-        // date must be of form YYYY-MM-DD (TODO natural-lang date processing)
+        // date must be of form YYYY-MM-DD (TODO date processing for natural lang input)
         Tasks.create({
             threadID: event.threadID,
             taskName: msg[3],
@@ -26,14 +37,14 @@ login({email: config.fb.e, password: config.fb.p}, function(err, api) {
             dueDate: moment(utils.parseDate(msg[5])).unix(),
           }, function(err, message) {
             if (err) return console.error(err);
-            // console.log('Task added')
-            api.sendMessage('Task added', event.threadID);
+            return api.sendMessage('Task added', event.threadID);
           }
         );
       } else if (msg.includes('show tasks')) {
+        // find tasks w/ '@taskbot show tasks', '@taskbot show tasks for "subject"'
         msg = msg.slice('show tasks'.length);
-        var query = {};
-        var subj;
+        var query = {}, 
+            subj;
 
         if (!utils.isEmpty(msg)) {
           subj = msg.slice(' for '.length).split('\"')[1];
@@ -43,15 +54,22 @@ login({email: config.fb.e, password: config.fb.p}, function(err, api) {
         Tasks.find(query, function(err, tasks) {
           if (err) return console.error(err);
           if (utils.isEmpty(tasks)) return api.sendMessage('No tasks found', event.threadID);
-
           tasks.forEach(function(task) {
-
             var out = task.taskName + ' | ' + moment.unix(task.dueDate).format('MMM Do, YYYY');
-            if (subj == null)
-              out = task.taskSubject + ' ' + out
-
+            if (subj == null) out = task.taskSubject + ' | ' + out
             api.sendMessage(out, event.threadID);
           });
+        });
+      } else if (msg.includes('remove ')) {
+        // remove tasks w/ '@taskbot remove "subject" "task name"'
+        msg = msg.slice('remove '.length).split('\"');
+        Tasks.remove({
+          taskSubject: msg[1],
+          taskName: msg[3]
+        }, function(err, count) {
+          if (err) return console.error(err);
+          if (count.result.n == 0) return api.sendMessage('Task not found', event.threadID);
+          return api.sendMessage('Task removed', event.threadID);
         });
       }
     }
